fcd17893f103ebac56f24edf906b928c5e12c0ef
thaliasu <thalia989231@knights.ucf.edu>
2019-03-07 12:24:44 -0500

    Minor Changes
    Edited writing and 404 animation.

--- js/page-not-found.js
+++ js/page-not-found.js
@@ -6,4 +6,9 @@ anime.timeline({
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
   duration: 2500
-});
+})
+.add({
+	targets: 'body',
+	backgroundColor: '#FDC134',
+	duration: 1,
+}, '-=10');
