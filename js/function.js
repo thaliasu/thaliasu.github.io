eeb2eeb162ab667ad689c41f45dedce25618d151
thaliasu <37873775+thaliasu@users.noreply.github.com>
2020-02-18 00:05:12 -0500

    Nav Rehaul

--- js/function.js
+++ js/function.js
@@ -1,45 +1,97 @@
 $(document).ready(function() {
+	//On page load, scroll to top
+	$(this).scrollTop(0);
+
+	//Scroll down to About when icon is clicked
 	$("#scroll-icon").click(function() {
 	    $([document.documentElement, document.body]).animate({
-	        scrollTop: $("#start-intro").offset().top,
+	        scrollTop: $("#about").offset().top,
 	        easing: 'swing'
 	    }, 1000);
 	});
 
+	//Scroll to section when nav link is clicked
 	$('a[href^="#"]').on('click', function(event) {
 	    var target = $(this.getAttribute('href'));
+	    var navHeight = $('nav').outerHeight(true);
+
 	    if( target.length ) {
 	        event.preventDefault();
 	        $('html, body').stop().animate({
-	            scrollTop: target.offset().top,
+	            scrollTop: target.offset().top - 82,
 	            easing: 'swing'
 	        }, 1000);
+	        if($(window).width() <= 632) {
+	    		$("nav").toggleClass("mobileIsActive");
+	    	}
 	    }
 	});
-});
 
-
-/*Experimental Code
-$(document).ready(function() {
-	$('#thalia').mouseover(function(){
-		$('#thalia').css({
-			'transform': 'translate(50px, 0px)',
-			'transition': 'all .6s ease'
-		});
+	//Scroll to top when welcome nav link is clicked
+	$("#welcome-link").click(function() {
+	    $([document.documentElement, document.body]).animate({
+	        scrollTop: 0,
+	        easing: 'swing'
+	    }, 1000);
+	    if($(window).width() <= 632) {
+    		$("nav").toggleClass("mobileIsActive");
+    	}
 	});
 
-	$('.my-rotate').click(function(){
-		$(this).css({
-			'transform': 'rotate(360deg)',
-			'transition': 'all .6s ease'
-		});
+	//Show and hide mobile nav
+	$("#nav-btn").click(function(){
+		$("nav").toggleClass("mobileIsActive");
+	});
+
+
+	//Show active link on click -- this experimental code doesn't work
+	// var linkClicked;
+	// $("#0").click(function(){
+	// 	linkClicked = 0;
+	// });
+	// $("#1").click(function(){
+	// 	linkClicked = 1;
+	// 	$('li').removeClass();
+	// 	$(this).addClass('active');
+	// });
+	// $("#2").click(function(){
+	// 	linkClicked = 2;
+	// });
+
+
+	//Controls active li class in nav
+	$(window).scroll(function(){
+		var navHeight = $('nav').outerHeight(true);
+		var windowHeight = $(window).height();
+
+		var windowTop = $(window).scrollTop();
+		var aboutTop = $('#about').offset().top;
+		var portfolioTop = $('#changing-purple').offset().top;
+
+		var passWelcome = windowHeight-navHeight;
+		var passAbout = aboutTop-navHeight;
+		var passPortfolio = portfolioTop-navHeight;
+
+		//Show active link when scrolling
+		if(windowTop < passWelcome) {
+			$('li').removeClass();
+			$('li:nth-child(1)').addClass('active');
+		} else if((windowTop >= passAbout) && (windowTop < passPortfolio)) {
+			$('li').removeClass();
+			$('li:nth-child(2)').addClass('active');
+		} else if(windowTop >= passPortfolio) {
+			$('li').removeClass();
+			$('li:nth-child(3)').addClass('active');
+		}
+
+		//Shrink Nav: When scrolling past top of About, decrease padding on nav
+		//If you use passAbout in the if statement, transition will be shaky as it recalculates passAbout with the new nav size
+		// if(windowTop >= aboutTop) {
+		// if(windowTop > passWelcome) {
+		if( (windowTop >= aboutTop-144) && ($(window).width() > 632) ) {
+			$('ul').addClass('shrink-nav');
+		} else {
+			$('ul').removeClass();
+		}
 	})
-
-	var offset = $('#portfolio').offset();
-	var top = offset.top;
-	var left = offset.left;
-
-	$( '#scroll-icon' ).click(function() {
-		$( 'window' ).offset({top: top, left: left});
-	});
-});*/
+});
--- js/my-anime.js
+++ js/my-anime.js
@@ -1,25 +1,29 @@
-var path = document.querySelector('path');
-var length = path.getTotalLength();
+// var path = document.querySelector('path');
+// var length = path.getTotalLength();
 
 anime.timeline({
   easing: 'easeOutExpo',
 })
 .add({
-	targets: '#nav-icon, #scroll-icon',
+	targets: '.nav-link',
 	opacity: [0,1],
-	translateY: [20,0],
-	duration: 10000
+	duration: 3000
 })
 .add({
   targets: '.morph',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
   duration: 2500,
-  /*delay: function(el, i) { return i * 250 },*/
-}, '-=10000')
+}, '-=2000')
+.add({
+  targets: '.morph-scroll-icon',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 2500,
+}, '-=2500')
 .add({
 	targets: 'body, nav',
 	backgroundColor: '#FDC134',
-	duration: 1,
-}, '-=7499')
+	duration: 1
+})
 ;
